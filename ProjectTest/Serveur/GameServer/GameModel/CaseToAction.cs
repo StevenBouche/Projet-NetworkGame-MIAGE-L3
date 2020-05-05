2344b191b08ff7de78813efe1c687800c9e9032c
steven <steven.bouche@outlook.com>
2020-05-05 22:53:30 +0200

    modif

--- ProjectTest/Serveur/GameServer/GameModel/CaseToAction.cs
+++ ProjectTest/Serveur/GameServer/GameModel/CaseToAction.cs
@@ -1,6 +1,7 @@
 ﻿using Serveur.GameServer.CommandPack;
 using Serveur.GameServer.Game;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,24 +9,31 @@ namespace Serveur.GameServer.GameModel
 {
     public class CaseToAction
     {
-        public static CaseToAction MISTERYACTION = new CaseToAction(typeof(CommandMystery), TypeCase.MYSTERE);
+        public static List<CaseToAction> listAction = new List<CaseToAction>();
+        public static CaseToAction MISTERYACTION = new CaseToAction(typeof(CommandMystery), TypeCase.MYSTERE, ref listAction);
+
         Type actionType;
         TypeCase caseType;
 
-        private CaseToAction(Type t, TypeCase t1)
+        private CaseToAction(Type t, TypeCase t1, ref List<CaseToAction> list)
         {
             this.actionType = t;
             this.caseType = t1;
+            list.Add(this);
         }
 
         public static Command<GameEngine> getCommand<T>(TypeCase caseType, T engine, CommandManager cmd) where T : GameEngine
         {
-            if(MISTERYACTION.caseType == caseType)
+
+            foreach(CaseToAction c in listAction)
             {
-                return new CommandMystery(engine, cmd);
+                if(c.caseType == caseType)
+                {
+                    return (Command<GameEngine>) Activator.CreateInstance(c.actionType, engine, cmd);
+                }
             }
-            
-            return null; //todo
+
+            return null;
         }
     }
 }
