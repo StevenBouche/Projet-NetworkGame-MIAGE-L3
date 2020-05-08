3bd2ae75edb81e575d3b1d1a7fda9d4bbfe1265e
Prevot <armand-prevot@hotmail.fr>
2020-05-08 17:04:33 +0200

    Adding proposal object

---
+++ ProjectTest/share/Network/Message/modele/Proposal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Share.Network.Message.modele
+{
+    public class Proposal 
+    {
+        String id;
+        String proposal;
+
+        public Proposal(String idP, String prop)
+        {
+            this.id = idP;
+            this.proposal = prop;
+        }
+    }
+}
